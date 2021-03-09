070e54f04c6951b82e9993a340f792131b89adb8
Ahmed Al-Refaie <ahmed.al-refaie.12@ucl.ac.uk>
2021-03-09 11:34:11 +0000

    Bump version: 0.0.2-dev0 → 0.1.0-dev0

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ files =  ([f[len('taurex_fastchem')+1:] for f in files])
 
 data_files = [*files, os.path.join('fastchem_src','chem_input','chemical_elements.dat') ]
 
-version = "0.0.2-dev0"
+version = "0.1.0-dev0"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
